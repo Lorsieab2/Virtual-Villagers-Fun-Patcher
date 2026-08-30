3df0afbbf9ba26ceb29c946c8ad1ae4f57370d62
Lorsieab2 <23109101+Lorsieab2@users.noreply.github.com>
2026-08-30 17:30:35 -0500

    Bump patcher release to v1.34.21

--- src/transparency.py
+++ src/transparency.py
@@ -19,7 +19,7 @@ from typing import Any, Iterable
 
 
 TRANSPARENCY_FILENAME = "VVFP Transparency Log.txt"
-PATCHER_VERSION = "v1.34.20"
+PATCHER_VERSION = "v1.34.21"
 
 
 def validate_feature_transparency_metadata(features: Iterable[Any]) -> None:
